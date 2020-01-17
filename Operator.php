a5516f62de3a6dd93410f9805a73ecdb057776f6
Rohitash Yadav <rohitashyadav455@gmail.com>
2020-01-17 18:35:00 -0500

    add new Operators

---
+++ Operator.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+   <form action="">
+   <label for="" >
+   
+   <input type="text" name="text1"><br><br>
+   <input type="text" name="text2"><br><br>
+   <input type="Sumbit">
+   </form><h3>
+    <h2></h2>
+    
+</body>
+</html>
+<?php
+
+?>
--- myfile.php
+++ myfile.php
@@ -35,13 +35,70 @@ echo "$dt1";
 $h="rohitash yadav";
 echo "<br><br>";
 echo "Length of string:<br>";
+
+
+/// lenght of string
 echo strlen($h);
 echo "<br>cout  of word of string:<br>";
+
+
+/// no of digit and character by using str_word_count
 echo str_word_count($h);
 echo "<br>reverse of string:<br>";
+
+/// reverse the string by using strrev function
 echo strrev($h);
 echo "<br>replace of string:<br>";
-echo str_replace("rohitash",,$h);
+echo str_replace("Hello"," ","Hello world");
+
+// is_int() function
+//is_float( ) function
+// casting
+$abcd=125.4;
+$intnum=(int)$abcd; // cast float into int
+
+echo "<br><br>";
+var_dump(is_float($abcd)); //check  wheather the variable is 
+ //float or not
+echo $intnum;
+
+
+//operators
+//1.Arithmetic operator
+//2.Relational operator
+//3.Logical operator
+//4.Assignment operator
+
+
+//1.Arithmetic operator
+//+, -, *, \, %, **;
+$a=45;
+$b=25;
+$c=4;
+$d=5;
+echo "<br><br>";
+echo "Addition of two no.:";
+echo $a+$b;
+
+echo "<br><br>";
+echo "Substraction of two no.:";
+echo $a-$b;
+
+echo "<br><br>";
+echo "Multiplication of two no.:";
+echo $a*$b;
+
+echo "<br><br>";
+echo "divition of two no.:";
+echo $a/$b;
+
+echo "<br><br>";
+echo "Module of two no.:";
+echo $a%$b;
+
+echo "<br><br>";
+echo "power of two no.:";
+echo $c**$d;
 
 
 
